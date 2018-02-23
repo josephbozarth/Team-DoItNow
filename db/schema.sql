33c34538d4908b95c1da9a4d627850f0c881fd41
josephbozarth <joseph.bozarth@ufl.edu>
2018-02-23 01:19:37 -0500

    added auto_increment to user_id fields in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS User;
 CREATE TABLE User (
-  user_id INTEGER PRIMARY KEY NOT NULL,
+  user_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
   email text NOT NULL, 
   password_hash text NOT NULL, 
   role text NOT NULL
@@ -8,7 +8,7 @@ CREATE TABLE User (
 
 DROP TABLE IF EXISTS Feature_Request;
 CREATE TABLE Feature_Request (
-  feature_id INTEGER PRIMARY KEY NOT NULL, 
+  feature_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
   user_id INTEGER NOT NULL, 
   name text NOT NULL, 
   description text NOT NULL, 
@@ -17,13 +17,13 @@ CREATE TABLE Feature_Request (
 
 DROP TABLE IF EXISTS Sprint;
 CREATE TABLE Sprint (
-  sprint_id INTEGER PRIMARY KEY NOT NULL, 
+  sprint_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
   name text NOT NULL
 );
 
 DROP TABLE IF EXISTS Story;
 CREATE TABLE Story (
-  story_id INTEGER PRIMARY KEY NOT NULL, 
+  story_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
   feature_id INTEGER NOT NULL, 
   user_id INTEGER NOT NULL, 
   sprint_id INTEGER NOT NULL, 
