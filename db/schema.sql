ae3453ec00a9532c986212b518f1b8f62214df45
WexCode4 <jwexler@ufl.edu>
2018-03-14 21:48:06 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -24,11 +24,11 @@ CREATE TABLE Sprint (
 DROP TABLE IF EXISTS Story;
 CREATE TABLE Story (
   story_id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT , 
-  feature_id INTEGER NOT NULL, 
+  feature_id INTEGER, 
   user_id INTEGER NOT NULL, 
   sprint_id INTEGER NOT NULL, 
   name text NOT NULL, 
-  description text NOT NULL, 
+  description text, 
   FOREIGN KEY (feature_id) REFERENCES Feature_Request(feature_id), 
   FOREIGN KEY (user_id) REFERENCES User(user_id), 
   FOREIGN KEY (sprint_id) REFERENCES Sprint(sprint_id)
